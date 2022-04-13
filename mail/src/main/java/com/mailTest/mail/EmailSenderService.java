84478f090db2829ad259c8c5d1422e613c7abcb3
cb-debajyotidebbarma <101862868+cb-debajyotidebbarma@users.noreply.github.com>
2022-04-13 16:04:16 +0530

    Add files via upload

--- mail/src/main/java/com/mailTest/mail/EmailSenderService.java
+++ mail/src/main/java/com/mailTest/mail/EmailSenderService.java
@@ -2,6 +2,7 @@ package com.mailTest.mail;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,13 @@ public class EmailSenderService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    String sendFrom;
+
     public void sendMail(Mail mail){
+
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("debajyoti.debbarma@chargebee.com");
+        simpleMailMessage.setFrom(sendFrom);
         simpleMailMessage.setTo(mail.getTo());
         simpleMailMessage.setSubject(mail.getSubject());
         simpleMailMessage.setText(mail.getBody());
